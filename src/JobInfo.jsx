f279b40cdd817a2d0bbde7070dc093ae964780aa
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-20 16:29:27 +0100

    create Duties component

--- src/JobInfo.jsx
+++ src/JobInfo.jsx
@@ -1,3 +1,5 @@
+import Duties from "./Duties";
+
 export const JobInfo = ({ jobs }) => {
   const { company, dates, duties, title } = jobs[0];
 
@@ -6,6 +8,7 @@ export const JobInfo = ({ jobs }) => {
       <h3>{title}</h3>
       <span className="job-company">{company}</span>
       <p className="job-date">{dates}</p>
+      <Duties duties={duties} />
     </article>
   );
 };
